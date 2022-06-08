6d69cc3ac0a98ebb25d7779fd2fead6b8ea9e3d6
jbravoh <jbravoharriott@gmail.com>
2022-06-08 14:13:48 +0100

    prisma module

--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -1,7 +1,12 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 
+// add global decorator
+@Global()
 @Module({
+  // PrismaService is imported in this module
   providers: [PrismaService],
+  // PrismaService is exported so AuthModule can access it
+  exports: [PrismaService],
 })
 export class PrismaModule {}
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService {}
+export class PrismaService extends PrismaClient {
+  constructor() {
+    // super calls the constructor we are extenting => Prisma Client
+    super({
+      datasources: {
+        db: {
+          // database url
+          url: 'postgresql://postgres:123@localhost:5434/nest?schema=public',
+        },
+      },
+    });
+  }
+}
